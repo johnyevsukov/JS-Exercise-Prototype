1f55a83de85217c5c52aead28247f62adff431e2
John Yevsukov <github.com@john.engineering>
2021-03-24 18:48:36 -0400

    solved all tasks

--- index.js
+++ index.js
@@ -39,15 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
   }
- 
- 
 
-  
-  
-  
+  Person.prototype.eat = function(food){
+    if(this.stomach.length < 10){
+      this.stomach.push(food);
+    }
+  }
+
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
+  }
   
   /*
     TASK 2
@@ -63,11 +73,25 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
-  
-  
+
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
+  }
+
+  Car.prototype.drive = function(distance){
+    this.tank -= (distance/this.milesPerGallon);
+    this.odometer += distance;
+    if(this.tank <= 0){
+      return `I ran out of fuel at ${(this.distance - (this.tank * this.tank) * this.milesPerGallon)} miles`
+    }
+  }
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -75,18 +99,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age)
+    this.favoriteToy = favoriteToy;
   }
+
+  Baby.prototype = Object.create(Person.prototype);
  
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  }
   
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. check if the function with this is called by new
+    2. check if the function with this is called by apply(), call(), or bind()
+    3. check if the function is called as a method
+    4. finally check if the function is called in the global scope (and if 'strict' enabled)
   */
   
   
